ca93cbd35b24c575910ee0b472cdd2ece73a33d9
RealRichi3 <molunorichie@gmail.com>
2023-09-24 00:42:59 +0100

    prefill email from cache on tribeRegistration

--- src/pages/TribePage.jsx
+++ src/pages/TribePage.jsx
@@ -35,6 +35,7 @@ const TribePage = () => {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     if (!currentUser) return
 
+    console.log(currentUser)
     setUser((prev) => {
       return { ...prev, ...currentUser }
     })
@@ -253,6 +254,7 @@ const TribePage = () => {
                 name="email"
                 type="email"
                 onChange={handleChange}
+                value={user.email}
                 required
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 h-12"
               />
